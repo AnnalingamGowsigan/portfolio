6de831526ab74cd8f4dee59e36d3964a9734597e
Gowsigan A <e18115@eng.pdn.ac.lk>
2023-03-15 21:44:32 +0530

    Add Home page.

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/AnnalingamGowsigan"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/annalingam-gowsigan-87a3991b4/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+      <a href="#" className="home__social-icon" target="_blank">
+        <i class="uil uil-dribbble"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
