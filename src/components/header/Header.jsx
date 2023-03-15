4bfa2b4e9a4f221d80c9207dd783b192cf0b4b1a
Gowsigan A <e18115@eng.pdn.ac.lk>
2023-03-15 16:09:45 +0530

    Add App.js

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import "./header.css";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+        <nav className="nav container">
+            <a href="index.html" className="nav__logo">Gowsi</a>
+
+            <div className="nav__menu">
+                <ul className="nav__list grid">
+                    <li className="nav__item">
+                        <a href="" className="nav link">
+                            <i className="uil uil- nav__icon"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
   )
 }
 
