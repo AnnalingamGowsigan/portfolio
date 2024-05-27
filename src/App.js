9d1b2242dc23585d8e9f046335a22c14a924cc61
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-05-27 12:57:11 +0530

    Remove unused Contact component from App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import ScrollUp from "./components/scrollup/ScrollUp";
 
@@ -19,7 +18,7 @@ const App = () => {
         {/* <Qualification /> */}
         {/* <Work /> */}
         {/* <Testimonials /> */}
-        <Contact />
+        {/* <Contact /> */}
       </main>
       <Footer />
       <ScrollUp />
